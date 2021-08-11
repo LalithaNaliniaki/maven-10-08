152a4797a3919f64b4496a1a3382f5bce4dd59e3
“vikas99341” <vikasisinlove@gmail.com>
2021-08-11 02:59:32 +0000

    two in one

--- git-demo/src/main/java/com/vikas/app/App.java
+++ git-demo/src/main/java/com/vikas/app/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
+        System.out.println( "Learning merge conflict");
         System.out.println( "New line added " );
     }
 }
