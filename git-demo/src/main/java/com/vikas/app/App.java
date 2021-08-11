a06bffb90341412d806b70cbc669545dc1ac56ab
vikas99341 <48900898+vikas99341@users.noreply.github.com>
2021-08-11 08:56:48 +0530

    updated App.java in feature branch

--- git-demo/src/main/java/com/vikas/app/App.java
+++ git-demo/src/main/java/com/vikas/app/App.java
@@ -8,8 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "changes from main branch");
-        System.out.println( "Learning merge conflict");
         System.out.println( "New line added " );
     }
 }
