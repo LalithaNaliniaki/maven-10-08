62c269b55d99430f4b46f39492f8b4fa549d35b6
LalithaNaliniaki <naliniaki2711@gmail.com>
2021-08-11 16:40:25 +0000

    commiting changes in main branch

--- git-demo/src/main/java/com/vikas/app/App.java
+++ git-demo/src/main/java/com/vikas/app/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "New line added " );
+        System.out.println( "New line to main branch" );
     }
 }
