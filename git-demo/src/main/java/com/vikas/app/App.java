35beb8553666ce8db7462ecf98540d5f57b0e3b4
LalithaNaliniaki <88653046+LalithaNaliniaki@users.noreply.github.com>
2021-08-12 17:23:10 +0530

    Updated App.java

--- git-demo/src/main/java/com/vikas/app/App.java
+++ git-demo/src/main/java/com/vikas/app/App.java
@@ -8,10 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-
-        System.out.println( "New Line added to Main Branch" )
-        System.out.println( "Learning Merge conflict" );
-        System.out.println( "New line added " );
         System.out.println( "New line to New feature branch" );
     }
 }
