de47573bef7f25367cae40ca7c919b167c5d8e82
LalithaNaliniaki <naliniaki2711@gmail.com>
2021-08-12 11:07:20 +0000

    New Code added to New-Feature

--- git-demo/src/main/java/com/vikas/app/App.java
+++ git-demo/src/main/java/com/vikas/app/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
+        System.out.println( "Learning Merge conflict" );
         System.out.println( "New line added " );
         System.out.println( "New line to New feature branch" );
     }
