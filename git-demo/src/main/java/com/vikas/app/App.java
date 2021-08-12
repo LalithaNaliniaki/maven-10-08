b6eec70c40600438984ec2c51bbdad0647aa948f
LalithaNaliniaki <naliniaki2711@gmail.com>
2021-08-12 10:46:38 +0000

    New Branch Featurebranch 3rd

--- git-demo/src/main/java/com/vikas/app/App.java
+++ git-demo/src/main/java/com/vikas/app/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "New line added " );
-        System.out.println( "New line to main branch for 2nd time" );
+        System.out.println( "New line to feature branch for 3rd time" );
     }
 }
